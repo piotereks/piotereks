61e227ad05ef68540e22be15e578d76973d7efec
Piotr Sakowski <piotereks@gmail.com>
2026-01-24 17:24:22 +0100

    more cleanup

--- vcf_react/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import VCFContactGenerator from './VCFContactGenerator';
-import './styles.css';
-
-function App() {
-  return <VCFContactGenerator />;
-}
-
-export default App;
